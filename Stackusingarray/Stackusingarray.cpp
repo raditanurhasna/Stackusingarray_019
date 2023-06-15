4f6b147fccd017261674a57fc785c4e22d9c8c5a
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-15 10:36:49 +0700

    Mengembalikan elemen yang ditambahkan (pushed) ke dalam tumpukan (stack).

--- Stackusingarray/Stackusingarray.cpp
+++ Stackusingarray/Stackusingarray.cpp
@@ -13,7 +13,18 @@ public:
 		top = -1;
 
 	}
+	string push(string element) {
+		if (top == 4) {
+			cout << "stack full" << endl;
+			return "";
+		}
 
+		top++;
+		stack_array[top] = element;
+		cout << endl;
+		cout << element << "ditambahkan(pushed)." << endl;
+
+		return element;
 	
 	}
 }
